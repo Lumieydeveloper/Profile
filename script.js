09843149c2c50f85f897c1e3460dfa18f9e4fc77
Lumieydeveloper <111233884+Lumieydeveloper@users.noreply.github.com>
2025-01-31 14:14:26 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Function to update UTC time dynamically
-function updateTime() {
-    const timeElement = document.querySelector('[data-testid="currentTimeUTC"]');
-    timeElement.textContent = new Date().toUTCString();
-}
-
-// Update time every second
-setInterval(updateTime, 1000);
-updateTime();
